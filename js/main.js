8ab9fefbf9a334e4696f70d5211b142a7742ce8d
Mihail Shestak <66316437+Mihail1186@users.noreply.github.com>
2020-08-24 20:09:35 +0300

    готов нижний слайдер+адаптив

--- js/main.js
+++ js/main.js
@@ -21,3 +21,28 @@ $(function(){
       ]
     });
 });
+
+
+$(function(){
+  $('.slider__footer').slick({
+     dots: true,
+     slidesToShow: 1,
+     slidesToScroll: 1,
+     nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="images/prev.svg" alt="next arrow"></button>',
+     prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="images/prev.svg" alt="prew arrow"></button>',
+     responsive: [
+       {
+         breakpoint: 1024,
+         settings: {
+
+         }
+       },
+       {
+         breakpoint: 640,
+         settings: {
+           arrows:false
+         }
+       }
+     ]
+   });
+});
