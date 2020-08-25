1ede54d39048dc4b1b2fc9464fba7caf6fba4b72
Mihail Shestak <66316437+Mihail1186@users.noreply.github.com>
2020-08-25 22:28:41 +0300

    ready footer

--- js/main.js
+++ js/main.js
@@ -1,48 +1,65 @@
-$(function(){
-   $('.top-slider').slick({
-      dots: true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="images/prev.svg" alt="next arrow"></button>',
-      prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="images/prev.svg" alt="prew arrow"></button>',
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
+$(function () {
+  $('.top-slider').slick({
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="images/prev.svg" alt="next arrow"></button>',
+    prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="images/prev.svg" alt="prew arrow"></button>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
 
-          }
-        },
-        {
-          breakpoint: 640,
-          settings: {
-            arrows:false
-          }
         }
-      ]
-    });
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          arrows: false
+        }
+      }
+    ]
+  });
 });
 
 
-$(function(){
+$(function () {
   $('.slider__footer').slick({
-     dots: true,
-     slidesToShow: 1,
-     slidesToScroll: 1,
-     nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="images/prev.svg" alt="next arrow"></button>',
-     prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="images/prev.svg" alt="prew arrow"></button>',
-     responsive: [
-       {
-         breakpoint: 1024,
-         settings: {
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="images/prev.svg" alt="next arrow"></button>',
+    prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="images/prev.svg" alt="prew arrow"></button>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
 
-         }
-       },
-       {
-         breakpoint: 640,
-         settings: {
-           arrows:false
-         }
-       }
-     ]
-   });
+        }
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          arrows: false
+        }
+      }
+    ]
+  });
 });
+
+function backToTop() {
+  let button = $('.back-to-top');
+
+  $(window).on('scroll', () => {
+    if ($(this).scrollTop() >= 50) {
+      button.fadeIn();
+    } else {
+      button.fadeOut();
+    }
+  })
+  button.on('click' , (e) => {
+    e.preventDefault();
+    $('html').animate({scrollTop:0}, 1000);
+  })
+}
+backToTop();
